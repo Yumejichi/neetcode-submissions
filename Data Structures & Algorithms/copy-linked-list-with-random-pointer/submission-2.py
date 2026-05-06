dfad95ada50d7c033758e3cbbd3cd3f2a71d9436
Yumejichi Fujita <fujita.yumejichi@gmail.com>
2026-05-05 20:53:10 -0700

    Add: copy-linked-list-with-random-pointer - submission-2

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-2.py
@@ -0,0 +1,44 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # if not head:
+        #     return None
+        # originalToNewMap = {}
+        # curr = head
+        # while curr:
+        #     new_node = Node(curr.val)
+        #     originalToNewMap[curr] = new_node
+        #     curr= curr.next
+
+        # new_head = originalToNewMap[head]
+
+        # curr = head
+        # while curr:
+        #     new_node = originalToNewMap[curr]
+        #     if curr.next:
+        #         new_node.next = originalToNewMap[curr.next]
+        #     if curr.random:
+        #         new_node.random = originalToNewMap[curr.random]
+        #     curr = curr.next
+
+        # return new_head
+
+        oldToCopy = collections.defaultdict(lambda: Node(0))
+        oldToCopy[None] = None
+
+        cur = head
+        while cur:
+            oldToCopy[cur].val = cur.val
+            oldToCopy[cur].next = oldToCopy[cur.next]
+            oldToCopy[cur].random = oldToCopy[cur.random]
+            cur = cur.next
+        return oldToCopy[head]
+
